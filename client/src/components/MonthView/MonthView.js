9984d3ea74754cb18f2fcbcdbe155c8ef00f83d9
SaifKbishi <39298208+SaifKbishi@users.noreply.github.com>
2021-05-12 04:34:59 +0300

    display tables

--- client/src/components/MonthView/MonthView.js
+++ client/src/components/MonthView/MonthView.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios' ;
 import './MonthView.css';
 import * as ReactBootStrap from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const MonthView =()=>{
  const [expense, setExpense] = useState([]);
@@ -26,47 +27,51 @@ const MonthView =()=>{
   setMonth(e.target.name= Number(e.target.value));
  }//handleChange
 
+ const renderAnExpense = (exp, index)=>{
+  return(
+   // <Link to={`/updateExpense/${exp._id}`}> 
+   <tr key={exp._id}>
+    <td>{exp.name}</td>
+    <td>{exp.amount}</td>
+    <td>{exp.expenseType}</td>
+    <td>{exp.description}</td>    
+   </tr>
+   // </Link>
+  );
+ }
+
  return(
   console.log('month view', month),
-  <div>    
-      {/* <p>hello from Month view</p> */}
-      <p>{month}</p>
-      <select onChange={handleChange}>
-       <option value="-1">select month</option>
-       <option value="1">January</option>
-       <option value="2">February</option>
-       <option value="3">March</option>
-       <option value="4">April</option>
-       <option value="5">May</option>
-       <option name="" value="6">June</option>
-       <option value="7">July</option>
-       <option value="8">August</option>
-       <option value="9">September</option>
-       <option value="10">October</option>
-       <option value="11">November</option>
-       <option value="12">December</option>
-      </select>
-      <hr/>
-      {
-       expense.map((oneExp=>{
-        return(
-         
-        <React.Fragment>
-         {/* <small>{oneExp.name}</small> */}
-         <tbody>
-           <tr>
-            <td>{oneExp.name}</td>
-            <td>{oneExp.amount}</td>
-            <td>{oneExp.description}</td>
-            <td>{oneExp.expenseType}</td>
-           </tr>
-          </tbody>
-
-        </React.Fragment>
-        );
-       }))
-      }
-      
+  <div>
+   <select onChange={handleChange}>
+    <option value="-1">select month</option>
+    <option value="1">January</option>
+    <option value="2">February</option>
+    <option value="3">March</option>
+    <option value="4">April</option>
+    <option value="5">May</option>
+    <option value="6">June</option>
+    <option value="7">July</option>
+    <option value="8">August</option>
+    <option value="9">September</option>
+    <option value="10">October</option>
+    <option value="11">November</option>
+    <option value="12">December</option>
+   </select>
+   <hr/>
+     <ReactBootStrap.Table striped bordered hover>
+      <thead>
+        <tr>
+         <th>Expense Name</th>
+         <th>Amount $</th>
+         <th>Expense Type</th>
+         <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+       {expense.map(renderAnExpense)     }
+      </tbody>
+     </ReactBootStrap.Table>    
   </div>
  );
 }//ExpensesTable
