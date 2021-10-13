d271c77e68ae9b7acd9d6b310da6d91da870e53c
SaifKbishi <39298208+SaifKbishi@users.noreply.github.com>
2021-10-13 06:28:39 +0300

    * route

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function initial(){
   });
  }//initial
 
-app.get('/',  (req, res) =>{  
+app.get('/*',  (req, res) =>{  
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 require('./src/routes/authRoutes')(app);
